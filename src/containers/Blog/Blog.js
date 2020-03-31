bbd5faa7fdb11192484460707af6ce65e6f8656a
devyako <yakhyoismoildjonov@gmail.com>
2020-03-31 10:31:47 +0500

    making single post clickable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
 import "./Blog.css";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import NewPost from "./NewPost/NewPost";
 import FullPost from "./FullPost/FullPost";
 
@@ -36,10 +36,11 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
-        <Route path=":id" component={FullPost} />
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" component={FullPost} />
+        </Switch>
       </div>
     );
   }
